8fa5682df8fd14c00105fe383c322a3ff831eb19
Seven Healer <rohan.chat321@gmail.com>
2025-01-25 18:56:07 +0530

    gitignore updated

--- query.py
+++ query.py
@@ -1,41 +1,41 @@
-from sentence_transformers import SentenceTransformer
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+    from sentence_transformers import SentenceTransformer
+    from pymongo import MongoClient
+    from dotenv import load_dotenv
+    import os
 
-load_dotenv()
+    load_dotenv()
 
-mongodb_uri = os.getenv("MONGO_URI")
-client = MongoClient(mongodb_uri)
-db = client['rag']
-rag_v0 = db['rag_v0']
+    mongodb_uri = os.getenv("MONGO_URI")
+    client = MongoClient(mongodb_uri)
+    db = client['rag']
+    rag_v0 = db['rag_v0']
 
-model = SentenceTransformer('all-miniLM-L6-v2')
+    model = SentenceTransformer('all-miniLM-L6-v2')
 
-query = "what is deepseek?"
+    query = "what is deepseek?"
 
-query_embedding = model.encode([query], convert_to_numpy=True)[0].tolist()
+    query_embedding = model.encode([query], convert_to_numpy=True)[0].tolist()
 
-pipeline = [
-    {
-        '$vectorSearch': {
-            'index': 'rag_search',
-            'path': 'embedding',
-            'queryVector': query_embedding,
-            'numCandidates': 384,
-            'limit': 3
+    pipeline = [
+        {
+            '$vectorSearch': {
+                'index': 'rag_search',
+                'path': 'embedding',
+                'queryVector': query_embedding,
+                'numCandidates': 384,
+                'limit': 3
+            }
+        }, {
+        '$project': {
+        '_id': 0, 
+        'text': 1, 
+        'score': {
+            '$meta': 'vectorSearchScore'
         }
-    }, {
-    '$project': {
-      '_id': 0, 
-      'text': 1, 
-      'score': {
-        '$meta': 'vectorSearchScore'
-      }
+        }
     }
-  }
-]
+    ]
 
-results = list(rag_v0.aggregate(pipeline))
+    results = list(rag_v0.aggregate(pipeline))
 
-print(results)
+    print(results)
